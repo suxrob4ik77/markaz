7045a319c5d18e0d6141050aa1aedebc69b2a25d
suxrob <suxrob4ik77>
2025-05-01 19:13:43 +0500

    sevee

--- config/urls.py
+++ config/urls.py
@@ -1,38 +1,8 @@
-# from django.contrib import admin
-# from django.urls import path, include
-# from rest_framework import permissions
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
-#
-# schema_view = get_schema_view(
-#    openapi.Info(
-#       title="Snippets API",
-#       default_version='v1',
-#       description="Test description",
-#       terms_of_service="https://www.google.com/policies/terms/",
-#       contact=openapi.Contact(email="contact@snippets.local"),
-#       license=openapi.License(name="BSD License"),
-#    ),
-#    public=True,
-#    permission_classes=(permissions.AllowAny,),
-# )
-#
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', include('configapp.urls')),  # Bo'sh joyni olib tashladik
-#     path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-#     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-#     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-# ]
-
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from configapp.views import *
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenBlacklistView)
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -46,12 +16,12 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('configapp.urls')),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('auth/logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    path('', include('configapp.urls')),  # Bo'sh joyni olib tashladik
     path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
