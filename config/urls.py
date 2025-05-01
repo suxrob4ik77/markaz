233770d3f5dfdeb91f85307bbf7421b6632a1a98
suxrob <suxrob4ik77>
2025-05-01 18:26:18 +0500

    seven

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
-
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -19,11 +16,11 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(' ', include('configapp.urls')),
+    path('', include('configapp.urls')),  # Bo'sh joyni olib tashladik
     path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-
